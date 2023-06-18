2449bc6e6cad03190097f2de5feda0d44dd33251
flamefks <118124715+flamefks@users.noreply.github.com>
2023-06-18 15:40:42 +0300

    update all views
    smth changed1
    
    changed2
    
    squash1
    
    last_com
    
    last_com

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ class ConfigData:
     driver_path= r"chromedriver-114.exe"
     base_email = 'test@protei.ru'
     base_passw = 'test'
+    local_m = "sdsd"
+    local_D = 'sdsd'
+    local_b = 'sdsdqdq'
+    g = "dsd"
