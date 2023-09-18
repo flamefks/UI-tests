6163993f5d03a2a508c568898a2d6fc976a12d61
filatov-a <118124715+flamefks@users.noreply.github.com>
2023-09-18 21:07:06 +0300

    delete all

--- config.py
+++ config.py
@@ -2,6 +2,3 @@ class ConfigData:
     driver_path= r"chromedriver-114.exe"
     base_email = 'test@protei.ru'
     base_passw = 'test'
-    local_m = "sdsd"
-    local_D = 'sdsd'
-    local_b = 'sdsdqdq'
